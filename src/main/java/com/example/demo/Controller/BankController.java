3ac3c6d8f29447ff37b264abb2e140fc86c2929f
Duja Abdurhman <134400247+DujaAbdulrhman@users.noreply.github.com>
2025-03-07 06:25:29 +0300

    Add files via upload

---
+++ src/main/java/com/example/demo/Controller/BankController.java
@@ -0,0 +1,13 @@
+package com.example.demo.Controller;
+
+import com.example.demo.Model.Customer;
+import lombok.Data;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankController {
+    //بخلصه اليوم بإذن الله
+}
+
